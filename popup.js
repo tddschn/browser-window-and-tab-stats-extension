7fbceb297d69df94afddc3251e764493ebb793ab
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-11-06 18:07:43 -0500

    2023-11-06T18-07-43Z

--- popup.js
+++ popup.js
@@ -1,11 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-  chrome.runtime.sendMessage({ message: "get_windows" }, (response) => {
-    const windowListElement = document.getElementById('windowList');
-    windowListElement.innerHTML = ''; // Clear the loading message
-    response.forEach(winInfo => {
-      const div = document.createElement('div');
-      div.textContent = `${winInfo.name}: ${winInfo.tabCount} tabs`;
-      windowListElement.appendChild(div);
+  chrome.windows.getCurrent({ populate: true }, currentWindow => {
+    chrome.windows.getAll({ populate: true }, (windows) => {
+      const windowListElement = document.getElementById('windowList');
+      const totalCountsElement = document.getElementById('totalCounts');
+      let totalTabs = 0;
+
+      // Clear the loading message
+      windowListElement.innerHTML = '';
+      totalCountsElement.innerHTML = '';
+
+      // Calculate total number of windows and tabs
+      const totalWindows = windows.length;
+      windows.forEach(win => totalTabs += win.tabs.length);
+
+      // Display total counts
+      totalCountsElement.textContent = `Windows: ${totalWindows} | Tabs: ${totalTabs}`;
+
+      // Iterate over the windows to display their details
+      windows.forEach(win => {
+        const div = document.createElement('div');
+        div.className = 'windowItem';
+        div.textContent = `${win.title}: ${win.tabs.length} tabs`;
+
+        // Highlight the current window
+        if (win.id === currentWindow.id) {
+          div.classList.add('current');
+        }
+
+        // Create a span for the window name to make it bold
+        const span = document.createElement('span');
+        span.className = 'windowName';
+        span.textContent = win.title.split(' - ')[0] || `Window ${win.id}`;
+        div.textContent = ''; // Clear the textContent before appending the child elements
+        div.appendChild(span);
+        div.appendChild(document.createTextNode(`: ${win.tabs.length} tabs`));
+
+        windowListElement.appendChild(div);
+      });
     });
   });
 });
