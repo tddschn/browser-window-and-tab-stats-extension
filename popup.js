dd98cc13182eafd6e55b7ff2c6e7ef6b74f699bc
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-11-06 18:18:36 -0500

    2023-11-06T18-18-36Z

--- popup.js
+++ popup.js
@@ -2,12 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
   chrome.windows.getCurrent({ populate: true }, currentWindow => {
     chrome.windows.getAll({ populate: true }, (windows) => {
       const windowListElement = document.getElementById('windowList');
-      const totalCountsElement = document.getElementById('totalCounts');
+      const totalWindowsElement = document.getElementById('totalWindows');
+      const totalTabsElement = document.getElementById('totalTabs');
       let totalTabs = 0;
       let totalWindows = 0;
 
+      // Clear the existing table contents except for the header
       windowListElement.innerHTML = '';
-      totalCountsElement.innerHTML = '';
 
       windows.forEach(win => {
         totalTabs += win.tabs.length;
@@ -16,13 +17,19 @@ document.addEventListener('DOMContentLoaded', () => {
         const activeTab = win.tabs.find(tab => tab.active);
         const windowName = activeTab ? activeTab.title : `Window ${win.id}`;
 
-        const div = document.createElement('div');
-        div.className = `windowItem ${win.id === currentWindow.id ? 'current' : ''}`;
-        div.innerHTML = `<span class='windowName'>${windowName}</span>: ${win.tabs.length} tabs`;
-        windowListElement.appendChild(div);
+        // Create a new row for each window
+        const tr = document.createElement('tr');
+        tr.className = `windowItem ${win.id === currentWindow.id ? 'current' : ''}`;
+        tr.innerHTML = `
+          <td class='windowName'>${windowName}</td>
+          <td>${win.tabs.length} tabs</td>
+        `;
+        windowListElement.appendChild(tr);
       });
 
-      totalCountsElement.textContent = `Windows: ${totalWindows} | Tabs: ${totalTabs}`;
+      // Set the total counts in the table header
+      totalWindowsElement.textContent = totalWindows;
+      totalTabsElement.textContent = totalTabs;
     });
   });
 });
